0a24a7108abda169fc08f7065b7a7392fe81d7b0
Tanvi Sharma <tanvi4504>
2024-08-28 11:28:14 +0530

    Javascript Assignment

---
+++ A3Q1-5.js
@@ -0,0 +1,39 @@
+                                        //    ARRAY METHODS
+
+// QUESTION 1
+
+let arr=[10,20,30,40];
+arr.push(1000);
+// console.log(arr);
+
+
+
+// QUESTION 2
+
+let arr1=[10,20,30,40,50];
+arr1.pop();
+// console.log(arr1);
+
+
+
+// QUESTION 3
+
+let arr2=[100,200,300,400];
+arr2.shift();
+// console.log(arr2);
+
+
+
+// QUESTION 4
+
+let arr3=[1,2,3,4,5];
+arr3.unshift(204);
+// console.log(arr3);
+
+
+
+// QUESTION 5
+
+let arr4=[10,20,30,40,50];
+arr4.splice(2,1,204);
+console.log(arr4);
